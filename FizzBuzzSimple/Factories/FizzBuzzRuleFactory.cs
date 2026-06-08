24761b2228a21a160375f7d6d4f1d7f4d5018b7c
Nishant <mca-reg202527062@timscdrmumbai.in>
2026-06-08 18:14:05 +0530

    Factory changes

--- FizzBuzzSimple/Factories/FizzBuzzRuleFactory.cs
+++ FizzBuzzSimple/Factories/FizzBuzzRuleFactory.cs
@@ -1,11 +1,10 @@
+using FizzBuzzSimple.Utilities;
+
 namespace FizzBuzzSimple.Factories;
 public class FizzBuzzRuleFactory : IFizzBuzzRuleFactory
 {
     public string? GetResult(int number)
     {
-        if (number % 15 == 0) return "FizzBuzz";
-        if (number % 3 == 0) return "Fizz";
-        if (number % 5 == 0) return "Buzz";
-        return null;
+        return FizzBuzzUtility.GetFizzBuzzResult(number);
     }
 }
